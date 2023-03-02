b7fb0bede48433601c188f22288e9aa763b2fbef
Sun-007 <umeshsavaliya007@gmail.com>
2023-03-02 18:47:02 +0530

    chore: done remove console

--- src/pages/dashboard/TableList.tsx
+++ src/pages/dashboard/TableList.tsx
@@ -3,16 +3,15 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../store/LeadStore";
 import TableItem from "./TableItem";
 
-const TableList:FC<{showModel:(id:string)=> void}>= ({showModel}) => {
-    const data = useSelector((state: RootState) => state.leads.data);
-    console.log("data hn", data)
-    return (<>
-        {data.map((value) => {
-            return (
-                <TableItem value={value} showModel={showModel} key={value.id} />
-            );
-        })}</>
-    );
+const TableList: FC<{ showModel: (id: string) => void }> = ({ showModel }) => {
+  const data = useSelector((state: RootState) => state.leads.data);
 
+  return (
+    <>
+      {data.map((value) => {
+        return <TableItem value={value} showModel={showModel} key={value.id} />;
+      })}
+    </>
+  );
 };
 export default TableList;
